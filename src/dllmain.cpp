d25022879fd7ae1b868b0e021ab5678d0684b491
take <takexaz@gmail.com>
2023-07-27 23:24:10 +0900

    fix switch

--- src/dllmain.cpp
+++ src/dllmain.cpp
@@ -16,8 +16,12 @@ BOOL APIENTRY DllMain(HMODULE hModule, DWORD  ul_reason_for_call, LPVOID lpReser
         };
         addState(zoom);
     }
-    case DLL_THREAD_ATTACH:
-    case DLL_THREAD_DETACH:
+    case DLL_THREAD_ATTACH: {
+        break;
+    }
+    case DLL_THREAD_DETACH: {
+        break;
+    }
     case DLL_PROCESS_DETACH:
         break;
     }
